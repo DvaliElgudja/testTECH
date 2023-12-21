b853782ea0baa4901cc8777855ac55b7c7b2d08d
DvaliElgudja <dvali.Elgudja@gmail.com>
2023-12-21 22:48:49 +0100

    on efface tout, pour recommencer from scratch

--- tests/genStr.test.ts
+++ tests/genStr.test.ts
@@ -15,69 +15,6 @@
 
 
 
-import { genStr } from '../src/genStr';
-
-describe('genStr function', () => {
-    test('met en place toutes les combinaisons des préfixes donnés dans l ordre spécifié', () => {
-        const prefixes = ['c', 'n', 's'];
-        const expectedOutput = ['c', 'cn', 'cns', 'cs', 'n', 'ns', 's'];
-        expect(genStr(prefixes)).toEqual(expectedOutput);
-    });
-
-    test('met en place toutes les combinaisons des préfixes donnés dans l ordre spécifié', () => {
-        const prefixes = ['c', 'c', 's'];
-        const expectedOutput = ['c', 'cc', 'ccs', 'cs', 'c', 'cs', 's'];
-        expect(genStr(prefixes)).toEqual(expectedOutput);
-    });
-
-    test('gère des préfixes contenant des majuscules', () => {
-        const prefixes = ['C', 'N', 'S'];
-        const expectedOutput = ['C', 'CN', 'CNS', 'CS', 'N', 'NS', 'S'];
-        expect(genStr(prefixes)).toEqual(expectedOutput);
-    });
-    
-    test('gère des préfixes mixant majuscules et minuscules', () => {
-        const prefixes = ['C', 'n', 'S'];
-        const expectedOutput = ['C', 'Cn', 'CnS', 'CS', 'n', 'nS', 'S'];
-        expect(genStr(prefixes)).toEqual(expectedOutput);
-    });
-    
-
-    
-    test('met en place toutes les combinaisons des préfixes donnés dans l ordre spécifié meme avec une lettre manquante', () => {
-        const prefixes = ['', 'n', 's'];
-        const expectedOutput = ['', 'n', 'ns', 's', 'n', 'ns', 's',];
-        expect(genStr(prefixes)).toEqual(expectedOutput);
-    });
-
-
-
-
-    test('gere un tableau vide', () => {
-        expect(genStr([])).toEqual([]);
-    });
-
-    test('gère des préfixes avec des caractères spéciaux', () => {
-        const prefixes = ['$', '%', '&'];
-        const expectedOutput = ['$', '$%', '$%&', '$&', '%', '%&', '&'];
-        expect(genStr(prefixes)).toEqual(expectedOutput);
-    });
-
-    test('gère des préfixes mixant lettres, chiffres et symboles', () => {
-        const prefixes = ['a', '2', '$'];
-        const expectedOutput = ['a', 'a2', 'a2$', 'a$', '2', '2$', '$'];
-        expect(genStr(prefixes)).toEqual(expectedOutput);
-    });
-
-  
-
-    
-    
-
-    
-});
-
-//je n'ai pas souvent utilisé les test dans mon parcours, j'ai eu recours au Google pour les premier pas et puis les test a la suite sont ajoutés par moi meme.
 
 
 
