8903a3c547d5e42989111b1b4b23f170597e074a
DvaliElgudja <dvali.Elgudja@gmail.com>
2023-12-27 18:16:49 +0100

    Correction des fautes.

--- function.test.ts
+++ function.test.ts
@@ -1,34 +1,27 @@
 import { charGenerator } from './function'; 
 
 describe('charGenerator', () => {
-    it('cela doit generer les bonne combinaisons avec les prfixes données', () => {
-        const prefixes = ['c','n','s'];
-        const expectedOutput = ['c','cn','cns','cs','n','ns','s'];
+    it('doit générer les bonnes combinaisons avec les préfixes donnés', () => {
+        const prefixes = ['c', 'n', 's'];
+        const expectedOutput = ['c', 'cn', 'cns', 'cs', 'n', 'ns', 's'];
         expect(charGenerator(prefixes)).toEqual(expectedOutput);
     });
 
-    it('ca doit renvoyer un tableau vide', () => {
+    it('doit renvoyer un tableau vide si aucun préfixe n est fourni', () => {
         const prefixes:string[] = [];
         const expectedOutput:string[] = [];
         expect(charGenerator(prefixes)).toEqual(expectedOutput);
     });
 
-    it('ca dois gerer les doublons', () => {
-        const prefixes = ['a','a','b'];
-        const expectedOutput = ['a','aa','aab','ab','a','ab','b'];
+    it('doit gérer les doublons', () => {
+        const prefixes = ['a', 'a', 'b'];
+        const expectedOutput = ['a', 'aa', 'aab', 'ab', 'a', 'ab', 'b'];
         expect(charGenerator(prefixes)).toEqual(expectedOutput);
     });
 
-    it('arrive a gerer les caracters speciaux', () => {
+    it('doit pouvoir gérer les caractères spéciaux', () => {
         const prefixes = ['$', '%', '&'];
-        const expectedOutput = ['$','$%','$%&','$&','%','%&','&'];
+        const expectedOutput = ['$', '$%', '$%&', '$&', '%', '%&', '&'];
         expect(charGenerator(prefixes)).toEqual(expectedOutput);
     });
-
-    it('', () => {
-        const prefixes = ['$', '%', '&'];
-        const expectedOutput = ['$','$%','$%&','$&','%','%&','&'];
-        expect(charGenerator(prefixes)).toEqual(expectedOutput);
-    });
-
 });
--- function.ts
+++ function.ts
@@ -1,26 +1,26 @@
 export function charGenerator(prefixes: string[]): string[] {
     let results: string[] = [];
 
-    // parcourours tous les préfixes
+    //  Parcourt tous les préfixes
     for (let i:number = 0; i < prefixes.length; i++) {
-        // ajoute le prefixe actuel aux résultats
+        // Ajoute le prefixe actuel aux résultats
         results.push(prefixes[i]);
 
-        // cree des combinaisons avec un autre prefixe
+        // Crée des combinaisons avec un autre préfixe
         for (let j:number = i + 1; j < prefixes.length; j++) {
-            // ajoute la combinaison des deux prefixes
+            // Ajoute la combinaison des deux prefixes
             results.push(prefixes[i] + prefixes[j]);
 
-            // avec les combinaison de deux ajoute le troisieme prefixe dans le cas ou cela exsite
+            // Avec les combinaisons de deux, ajoute le troisième préfixe dans le cas où cela existe
             for (let k:number = j + 1; k < prefixes.length; k++) {
                 results.push(prefixes[i] + prefixes[j] + prefixes[k]);
 
-                // dans le cas ou nous rajoutons un 4eme prefixe.
-                // for (let l:number = k + 1; l < prefixes.length; l++) {
+                // Dans le cas où nous rajoutons un 4ème préfixe
+                // for (let l: number = k + 1; l < prefixes.length; l++) {
                 //     results.push(prefixes[i] + prefixes[j] + prefixes[k] + prefixes[l]);
             }
         }
     }
-    //renvoi le resultat
+    
     return results;
 }
